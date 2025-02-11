00e92c1dea7fc88c3bcc846acd4d86a58a4fbd8e
Bettina <bettike18989@gmail.com>
2025-02-11 20:39:47 +0100

    Project3

--- src/components/NavBar/BurgerMenu.jsx
+++ src/components/NavBar/BurgerMenu.jsx
@@ -59,7 +59,7 @@ const BurgerMenu = ({ navBarConfig, currentLang, cartCount, count, activeLink, t
 
             {/* Login/Logout Button */}
             <div className="mt-6">
-                {token(
+                {token && (
                     <button
                         className="text-gray-900 bg-green-200 hover:bg-green-400 p-3 font-semibold rounded transition-all duration-300 ease-in-out"
                         onClick={onLogOut}
